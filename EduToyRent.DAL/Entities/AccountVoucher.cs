36ec2718f083bdc7706636feac96e5ed4c0b0055
Quyền Lê <daiquyen0@gmail.com>
2024-09-27 23:29:48 +0700

    Update entities
    pre-write entities for codefirst

---
+++ EduToyRent.DAL/Entities/AccountVoucher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduToyRent.DAL.Entities
+{
+    public class AccountVoucher
+    {
+        public int AccountId { get; set; }
+        public int VoucherId { get; set; }
+        public bool IsUsed { get; set; }
+
+        [ForeignKey("AccountId")]
+        public virtual Account Account { get; set; }
+
+        [ForeignKey("VoucherId")]
+        public virtual Voucher Voucher { get; set; }
+    }
+}
---
+++ EduToyRent.DAL/Entities/DepositOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduToyRent.DAL.Entities
+{
+    public class DepositOrder
+    {
+        [Key]
+        public int DepositOrderId { get; set; }
+        public int OrderId { get; set; }
+        public decimal TotalMoney { get; set; }
+        public decimal RefundMoney { get; set; }
+        public string BankCode { get; set; }
+        public string BankName { get; set; }
+        public DateTime DepositDate { get; set; }
+        public DateTime? RefundDate { get; set; }
+        public int StatusOrder { get; set; }
+
+        [ForeignKey("OrderId")]
+        public virtual Order Order { get; set; }
+    }
+}
