64967bd701deaca1c2a86889e1d5d8d6b823e585
phanvongocphu <155410531+phanvongocphu@users.noreply.github.com>
2024-10-10 15:42:22 +0700

    Revert "CC"
    This reverts commit b548456c3317ecb6d8f5a43b301c6fe7259f053b.

--- EduToyRent.API/Controllers/ToyController.cs
+++ EduToyRent.API/Controllers/ToyController.cs
@@ -87,7 +87,6 @@ namespace EduToyRent.API.Controllers
             return Ok(result);
         }
 
-
         [HttpGet("SortToysForSale")]
         public async Task<IActionResult> SortToysForSale(string sortBy, int pageIndex = 0, int pageSize = 10)
         {
