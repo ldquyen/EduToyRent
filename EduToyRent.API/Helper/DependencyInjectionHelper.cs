d1b0a6dc1697e5f49cdd4d192a39c22b0f431522
bacsiemon <johnsieunhan816@Gmail.com>
2024-10-12 13:27:31 +0700

    cart fix

--- EduToyRent.API/Helper/DependencyInjectionHelper.cs
+++ EduToyRent.API/Helper/DependencyInjectionHelper.cs
@@ -30,8 +30,10 @@ namespace EduToyRent.API.Helper
 			services.AddScoped<ICartService, CartService>();
             services.AddScoped<IOrderService, OrderService>();  
             services.AddScoped<IVoucherService, VoucherService>();
-          
-            return services;
+
+			return services;
         }
+
+
     }
 }
