8f6c86dcfae55293eb8c23f126acec4afb88c886
phanvongocphu <155410531+phanvongocphu@users.noreply.github.com>
2024-10-26 11:07:18 +0700

    Combine View, Search, Sort

--- EduToyRent.Service/Interfaces/IToyService.cs
+++ EduToyRent.Service/Interfaces/IToyService.cs
@@ -15,13 +15,9 @@ namespace EduToyRent.Service.Interfaces
         Task<dynamic> CreateSaleToy(CreateSaleToyDTO createSaleToyDTO, string fileURL, int supplierID);
         Task<dynamic> GetToyByToyId(int toyId);
         Task<Result> UpdateToyInfo(int toyId, UpdateToyDTO updateToyDTO);
-        Task<Pagination<ViewToyForRentDTO>> ViewToysForRent(int pageIndex, int pageSize);
-        Task<Pagination<ViewToyForSaleDTO>> ViewToysForSale(int pageIndex, int pageSize);
         Task<ViewToyForRentDetailDTO> ViewToyDetailForRent(int toyId);
         Task<ViewToyForSaleDetailDTO> ViewToyDetailForSale(int toyId);
-        Task<Pagination<ViewToyForRentDTO>> SearchRentByName(string keyword, int pageIndex, int pageSize);
-        Task<Pagination<ViewToyForSaleDTO>> SearchSaleByName(string keyword, int pageIndex, int pageSize);
-        Task<Pagination<ViewToyForRentDTO>> SortToysForRent(string sortBy, int pageIndex, int pageSize);
-        Task<Pagination<ViewToyForSaleDTO>> SortToysForSale(string sortBy, int pageIndex, int pageSize);
+        Task<Pagination<ViewToyForSaleDTO>> ViewToysForSale(string search, string sort, int pageIndex, int pageSize);
+        Task<Pagination<ViewToyForRentDTO>> ViewToysForRent(string search, string sort, int pageIndex, int pageSize);
     }
 }
