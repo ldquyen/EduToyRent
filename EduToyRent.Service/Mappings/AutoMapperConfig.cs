ff70d352100812c67512dffee0d780ca68fc0df1
Quyền Lê <daiquyen0@gmail.com>
2024-11-09 03:16:44 +0700

    update info ship

--- EduToyRent.Service/Mappings/AutoMapperConfig.cs
+++ EduToyRent.Service/Mappings/AutoMapperConfig.cs
@@ -62,13 +62,15 @@ namespace EduToyRent.Service.Mappings
                 .ForMember(dest => dest.AccountName, opt => opt.MapFrom(src => src.Account.AccountName))
                 .ForMember(dest => dest.StatusName, opt => opt.MapFrom(src => src.StatusOrder.StatusName));
             CreateMap<OrderDetail, ODRentDTO>()
-                .ForMember(dest => dest.ToyName, opt => opt.MapFrom(src => src.Toy.ToyName));
+                .ForMember(dest => dest.ToyName, opt => opt.MapFrom(src => src.Toy.ToyName))
+                .ForMember(dest => dest.ShipperPhone, opt => opt.MapFrom(src => src.ShipDates.ShipperPhone));
 
             CreateMap<Order, ResponseOrderSaleDetailForUserDTO>()
                  .ForMember(dest => dest.AccountName, opt => opt.MapFrom(src => src.Account.AccountName))
                 .ForMember(dest => dest.StatusName, opt => opt.MapFrom(src => src.StatusOrder.StatusName));
             CreateMap<OrderDetail, ODSaleDTO>()
-                .ForMember(dest => dest.ToyName, opt => opt.MapFrom(src => src.Toy.ToyName));
+                .ForMember(dest => dest.ToyName, opt => opt.MapFrom(src => src.Toy.ToyName))
+                .ForMember(dest => dest.ShipperPhone, opt => opt.MapFrom(src => src.ShipDates.ShipperPhone));
             CreateMap<Order, ResponseOrderForUser>()
             .ForMember(dest => dest.StatusName, opt => opt.MapFrom(src => src.StatusOrder.StatusName));
 
